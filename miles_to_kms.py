35114cab034a43edd15764e4c4bb8056d7c3222b
SuhasKulkarni5180 <35097378+SuhasKulkarni5180@users.noreply.github.com>
2020-02-23 10:24:23 +0530

    Initial code

---
+++ miles_to_kms.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+# This program converts miles to Kms
+
+print("Convert miles to Kms")
+miles=float(input("Enter total miles to want to convert :"))
+
+# 1 Mile =1.609344 Kms
+
+Kms= miles*1.609344
+Kms=round(Kms,4)
+print(miles,"miles is equal to",Kms,"Kilometers")
+ 
+
